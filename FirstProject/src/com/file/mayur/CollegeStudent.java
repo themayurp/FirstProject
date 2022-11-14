3ff8954e3010b1e7e5f02be822c20d8c613a351d
MAYUR PATIL <m.patil517152@gmail.com>
2022-11-14 20:34:26 +0530

    hello 2nd person

--- FirstProject/src/com/file/mayur/CollegeStudent.java
+++ FirstProject/src/com/file/mayur/CollegeStudent.java
@@ -1,5 +1,9 @@
 package com.file.mayur;
 
 public class CollegeStudent {
+	
+	public void collegeStudent() {
+		System.out.println("Hello I am a College Student");
+	}
 
 }
--- FirstProject/src/com/file/mayur/Person.java
+++ FirstProject/src/com/file/mayur/Person.java
@@ -1,9 +1,21 @@
 package com.file.mayur;
 
-public class Person {
+public class Person extends CollegeStudent{
+	
+	public void student() {
+		System.out.println("Hello I am a Student");
+	}
+	
+	public void employee() {
+		System.out.println("Hello I am an Employee");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Person p = new Person();
+		p.student();
+		p.employee();
+		p.collegeStudent();
 
 	}
 
